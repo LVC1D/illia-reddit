3dccfe850926abc76f72b2e7816474a8dfe22d82
Hector Cryo <sonic200887@gmail.com>
2024-03-10 11:59:14 +0200

    Set up the file structure for the app

---
+++ src/app/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>This will be a Reddit thing!</h1>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, {Root} from 'react-dom/client';
+import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
